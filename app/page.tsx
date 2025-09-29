964ca8ae8871cdc0adc6bd0977d75a432c9b727c
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2025-09-29 21:11:32 +0700

    update delelete props

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import InvoiceStatus from "./ui/invoices/status";
 import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 
-export interface Props {
-  myName: string;
-}
-
-export default function Page({ myName = "John" }: Props) {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -26,7 +22,7 @@ export default function Page({ myName = "John" }: Props) {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <b>My name is: {myName}</b>
+            <b>Hello World!</b>
           </p>
           <InvoiceStatus status={"paid"} />
           <Link
